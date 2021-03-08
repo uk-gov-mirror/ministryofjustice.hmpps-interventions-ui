f903b91346352fa509e66b570f9d0710982a8363
George Eaton <george@dxw.com>
2021-03-08 16:40:01 +0000

    Add textarea args to add activities view
    This will render a govuk textarea for each desired outcome selected on
    the referral.
    
    To think about: should we pass the nunjucks macro into the view here,
    like we did for the Intervention Details view? I initially went down
    this route, but didn't want to pass things like the enclosing form input
    into the view as well, so reverted to this implementation.
    
    Still to come:
    - Render any activities that have already been set above
    the textarea.
    - Validations on the texarea submit

--- server/routes/serviceProviderReferrals/addActionPlanActivitiesView.ts
+++ server/routes/serviceProviderReferrals/addActionPlanActivitiesView.ts
@@ -8,7 +8,22 @@ export default class AddActionPlanActivitiesView {
       'serviceProviderReferrals/actionPlan/addActivities',
       {
         presenter: this.presenter,
+        addActivityTextareaArgs: this.addActivityTextareaArgs,
       },
     ]
+  }
+
+  private addActivityTextareaArgs(index: number, desiredOutcomeDescription: string) {
+    return {
+      name: `add-activity-desired-outcome-${index}`,
+      id: `add-activity-desired-outcome-${index}`,
+      label: {
+        html: `<h3 class="govuk-heading-m">Desired Outcome ${index}</h3><p class="govuk-body-m">${desiredOutcomeDescription}</p>`,
+        classes: 'govuk-label',
+      },
+      hint: {
+        text: 'What activity will you deliver to achieve this outcome?',
+      },
+    }
   }
 }
