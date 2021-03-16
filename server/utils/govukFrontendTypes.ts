3d17dfa6cb2b7b45c840bd3a1d534b98e61dd114
Lawrence Forooghian <lawrence@dxw.com>
2021-03-16 13:03:30 +0000

    Hide create action plan button if one already exists

--- server/utils/govukFrontendTypes.ts
+++ server/utils/govukFrontendTypes.ts
@@ -37,7 +37,7 @@ export interface SummaryListArgs {
   rows: SummaryListRow[]
 }
 
-interface SummaryListRow {
+export interface SummaryListRow {
   key: { text: string }
   value: { html?: string; text?: string }
 }
