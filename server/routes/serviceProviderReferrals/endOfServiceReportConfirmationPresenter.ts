dce9bd0e1a3530bd14db1055822fad613c3dbe74
Lawrence Forooghian <lawrence@dxw.com>
2021-04-19 20:07:44 +0100

    SP user sees confirmation page after submitting end of service report

---
+++ server/routes/serviceProviderReferrals/endOfServiceReportConfirmationPresenter.ts
@@ -0,0 +1,29 @@
+import { SentReferral, ServiceCategory } from '../../services/interventionsService'
+import PresenterUtils from '../../utils/presenterUtils'
+import { SummaryListItem } from '../../utils/summaryList'
+import utils from '../../utils/utils'
+
+export default class EndOfServiceReportConfirmationPresenter {
+  constructor(private readonly referral: SentReferral, private readonly serviceCategory: ServiceCategory) {}
+
+  progressHref = `/service-provider/referrals/${this.referral.id}/progress`
+
+  // Copied from ActionPlanConfirmationPresenter
+  readonly summary: SummaryListItem[] = [
+    {
+      key: 'Name',
+      lines: [PresenterUtils.fullName(this.referral.referral.serviceUser)],
+      isList: false,
+    },
+    {
+      key: 'Referral number',
+      lines: [this.referral.referenceNumber],
+      isList: false,
+    },
+    {
+      key: 'Service type',
+      lines: [utils.convertToProperCase(this.serviceCategory.name)],
+      isList: false,
+    },
+  ]
+}
