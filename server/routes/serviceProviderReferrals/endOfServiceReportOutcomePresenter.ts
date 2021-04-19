b41a4776502ce337cabe73ee6162404a256cbcbf
Lawrence Forooghian <lawrence@dxw.com>
2021-04-19 19:55:05 +0100

    SP user can enter outcomes in end of service report

---
+++ server/routes/serviceProviderReferrals/endOfServiceReportOutcomePresenter.ts
@@ -0,0 +1,56 @@
+import {
+  DesiredOutcome,
+  EndOfServiceReport,
+  EndOfServiceReportOutcome,
+  SentReferral,
+  ServiceCategory,
+} from '../../services/interventionsService'
+import { FormValidationError } from '../../utils/formValidationError'
+import PresenterUtils from '../../utils/presenterUtils'
+
+export default class EndOfServiceReportOutcomePresenter {
+  constructor(
+    private readonly referral: SentReferral,
+    private readonly endOfServiceReport: EndOfServiceReport,
+    private readonly serviceCategory: ServiceCategory,
+    private readonly desiredOutcome: DesiredOutcome,
+    private readonly desiredOutcomeNumber: number,
+    private readonly outcome: EndOfServiceReportOutcome | null,
+    private readonly userInputData: Record<string, unknown> | null = null,
+    private readonly error: FormValidationError | null = null
+  ) {}
+
+  readonly errorSummary = PresenterUtils.errorSummary(this.error)
+
+  readonly text = {
+    // TODO this should be the 'Social inclusion: end of service report' and on previous screen too
+    title: `About desired outcome ${this.desiredOutcomeNumber}`,
+    subtitle: `Desired outcome ${this.desiredOutcomeNumber}`,
+    desiredOutcomeDescription: this.desiredOutcome.description,
+    achievementLevel: {
+      label: `Overall, did ${this.referral.referral.serviceUser.firstName} achieve desired outcome ${this.desiredOutcomeNumber}?`,
+    },
+  }
+
+  private readonly utils = new PresenterUtils(this.userInputData)
+
+  readonly fields = {
+    achievementLevel: {
+      errorMessage: PresenterUtils.errorMessage(this.error, 'achievement-level'),
+      values: [
+        { text: 'Achieved', value: 'ACHIEVED' },
+        { text: 'Partially achieved', value: 'PARTIALLY_ACHIEVED' },
+        { text: 'Not achieved', value: 'NOT_ACHIEVED' },
+      ].map(option => ({
+        ...option,
+        selected: this.utils.stringValue(this.outcome?.achievementLevel ?? null, 'achievement-level') === option.value,
+      })),
+    },
+    progressionComments: {
+      value: this.utils.stringValue(this.outcome?.progressionComments ?? null, 'progression-comments'),
+    },
+    additionalTaskComments: {
+      value: this.utils.stringValue(this.outcome?.additionalTaskComments ?? null, 'additional-task-comments'),
+    },
+  }
+}
